a0b42509a5895900f2b39026b2d3dcbdc22f8001
Damon <damon.j.nunez@gmail.com>
2023-09-18 18:11:25 -0400

    Icons Adds for Skills.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import './Skills.css'
 import {Row,Col,Container} from 'react-bootstrap'
+import {BiLogoHtml5} from 'react-icons/bi'
+import {BiLogoCss3} from 'react-icons/bi'
+import {BiLogoJavascript} from 'react-icons/bi'
+import {BiLogoReact} from 'react-icons/bi'
+import {BiLogoBootstrap} from 'react-icons/bi'
 const Skills = () => {
   return (
     <div className='skillsSection'>
@@ -16,11 +21,37 @@ const Skills = () => {
                 <Col sm={12} md={12} lg={6}>
                 <div className='outerBorder'>
                     <h1 className='skillType'>Front-End!</h1>
+                <div className='iconic-container'>
+                    <div className='iconic'>
+                  <BiLogoHtml5 size='5rem' color='darkorange'/>
+                  <p className='caption'>HTML5</p>
+                 </div>
+
+                    <div className='iconic'>
+                    <BiLogoCss3 size='5rem' color='blue'/>
+                    <p className='caption'>CSS</p>
+                    </div>
+
+                    <div className='iconic'>
+                    <BiLogoJavascript size='5rem' color='yellow'/>
+                    <p className='caption'>JavaScript</p>
+                    </div>
+                </div>
+                <div className='iconic-container'>
+                <div className='iconic'>
+                    <BiLogoReact size='5rem' color='lightblue'/>
+                    <p className='caption'>React</p>
+                    </div>
+                    <div className='iconic'>
+                        <BiLogoBootstrap size='5rem' color='purple'/>
+                        <p className='caption'>Bootstrap</p>
+                    </div>
+                </div>
                 </div>
                 </Col>
                 <Col sm={12} md={12} lg={6}>
                     <div className='outerBorder'>
-
+                        <h1 className='skillType'>Back-End!</h1>
                     </div>
                 </Col>
             </Row>
